2a5f18d6b3702f9b45967ff4447fce70013f1bed
mohitrawal0424 <112812307+mohitrawal0424@users.noreply.github.com>
2023-06-29 08:55:04 +0530

    some changes

---
+++ include/modals/lend_more_principal_modal.php
@@ -0,0 +1,40 @@
+<?php
+
+echo '<div id="myModallendprinciple" class="modal hidden fixed inset-0 flex items-center justify-center z-50">
+// <div class="modal-overlay outsidemodal absolute w-full h-full bg-gray-900 opacity-50"></div>
+<div class="modal-content bg-white text-gray-800 rounded shadow-lg w-1/2">
+  
+<form action="" method="POST">
+              <div class="grid gap-4 sm:grid-cols-2 sm:gap-6 p-4">
+                  <div>
+                      <label for="dorepay-principall" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">D.O.Repayment</label>
+                      <input type="date" name="dorepay-principall" id="dorepay-principall" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+                  </div>
+                  <div>
+                      <label for="loan_idl" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Loan ID</label>
+                      <input type="number" name="loan_idl" id="loan_idl" value="' . $row['id'] . '" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required readonly>
+                  </div>
+                  <div>
+                      <label for="loantype" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Loan Type</label>
+                      <input type="text" name="loantype" id="loantype" value="CC Loan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required readonly>
+                  </div>
+                  <div>
+                      <label for="principle-amount-repayl" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Principle Amount</label>
+                      <input type="number" name="principle-amount-repayl" id="principle-amount-repayl" value="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+                  </div>
+                  <div>
+                      <label for="comment_prirepayl" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">Comment</label>
+                      <input type="text" name="comment_prirepayl" id="comment_prirepayl" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+                  </div>
+
+                  <div id="principlelendalert"></div>
+                  <button type="submit" id="lend-principle-submitbtnn" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                      Give Principle
+                  </button>
+              </div>
+          </form>
+  
+</div>
+</div>';
+
+?>
---
+++ include/modals/tables_modals/principal_lend_paid_table.php
@@ -0,0 +1,43 @@
+<?php
+
+echo '<div id="paidprincipaltableModal" class="fixed inset-0 flex items-center justify-center z-50 hidden">
+  <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+  
+  <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+    <!-- Modal Content -->
+    <div class="modal-content py-4 text-left px-6">
+      <!-- Close Button/Icon -->
+
+      <button id="closeprincipaltableModal" class="close-button border bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+      <i class="fa-solid fa-xmark"></i>
+</button>
+      <table class="text-left w-full">
+		<thead class="bg-black flex text-white w-full">
+			<tr class="flex w-full mb-4">
+				<th class="p-4 w-1/4">Date</th>
+				<th class="p-4 w-1/4">Installment</th>
+			</tr>
+		</thead>
+    <!-- Remove the nasty inline CSS fixed height on production and replace it with a CSS class — this is just for demonstration purposes! -->
+		<tbody class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style="height: 30vh;">
+			';
+      require_once "../connect.php";
+      
+      $sql3 = "SELECT dorepayment,repay_amount FROM `principle_repayment` where loan_id=$loanid";
+      $result3 = mysqli_query($conn,$sql3);
+      if (mysqli_num_rows($result3) > 0) {
+        while ($row3 = mysqli_fetch_assoc($result3)) {
+
+          echo '<tr class="flex w-full mb-4">
+          <td class="p-4 w-1/4 font-bold">'.$row3['dorepayment'].'</td>
+          <td class="p-4 w-1/4 font-bold">'.$row3['repay_amount'].'</td></tr>';
+            }
+          }
+				echo'</tbody>
+  </table>
+  </div>
+  </div>
+  </div>
+</div>';
+
+?>
