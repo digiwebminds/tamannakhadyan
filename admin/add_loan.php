4c8142a3cc63cc7452a4c633cdb4a9a0412ee9aa
Digvijay Singh Bisht <vickybisht524@gmail.com>
2023-07-14 22:20:30 +0530

    Shop name while adding loan

--- admin/add_loan.php
+++ admin/add_loan.php
@@ -148,6 +148,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <label for="customer-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Customer Name</label>
                         <input type="text" name="customer-name" id="customer-name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required readonly>
                     </div>
+                    <div class="sm:col-span-2">
+                        <label for="customer-shop" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Shop Name</label>
+                        <input type="text" name="customer-shop" id="customer-shop" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required readonly>
+                    </div>
                     <div id="lcdependent" class="sm:col-span-2">
                         <!-- <div class="w-full">
                         <label for="principle-amount" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Principle Amount</label>
